303d0f4e4c16820730ee7256e2cfc3b595a08883
Granto1 <63463218+Granto1@users.noreply.github.com>
2023-02-28 19:47:24 -0500

    Grant drive train (#1)
    * 2/26 Setup for initial testing
    
    * Quick Fix before shifting to Prog7
    
    * 2/26 Initial Testing
    
    Notes:
    Solenoid allocation error (channels don't overlap so need to do some reading)
    Solenoids were working
    Drivetrain works
    
    * 2/26 More Tinkering
    
    More To-Do:
    Commands for Half/Quarter Speed
    Navx connection...
    
    * 2/26 To Test + RobotBuilder Integration
    
    * 2/28 Drivetrain works, Claw and Shifter Solenoids Function
    
    Buttons will be swapped around once drive team is established
    
    * 2/28 Updated Shifters to reflect shift change
    
    * 2/28 Formatted, fixed QuarterSpeed HalfSpeed Logic
    
    Some Comments added too

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -5,15 +5,25 @@
 package frc.robot;
 
 /**
- * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
- * constants. This class should not be used for any other purpose. All constants should be declared
+ * The Constants class provides a convenient place for teams to hold robot-wide
+ * numerical or boolean
+ * constants. This class should not be used for any other purpose. All constants
+ * should be declared
  * globally (i.e. public static). Do not put anything functional in this class.
  *
- * <p>It is advised to statically import this class (or one of its inner classes) wherever the
+ * <p>
+ * It is advised to statically import this class (or one of its inner classes)
+ * wherever the
  * constants are needed, to reduce verbosity.
  */
-public final class Constants {
-  public static class OperatorConstants {
-    public static final int kDriverControllerPort = 0;
-  }
+public class Constants {
+
+   public static final class DriveConstants {
+      public static final int kLeftMotor1Port = 16; // LeftFront
+      public static final int kLeftMotor2Port = 17; // LeftRear
+      public static final int kRightMotor1Port = 3; // RightFront
+      public static final int kRightMotor2Port = 2; // RightRear
+      public static final int shifterChannel = 1; // Shifter
+      public static final int clawChannel = 2; // Claw
+   }
 }
