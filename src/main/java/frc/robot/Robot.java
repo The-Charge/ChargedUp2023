e13ad83984fedfd714f04c3c8886b4bfb736f69f
Grant Wang <grantoyt@gmail.com>
2023-03-13 18:28:14 -0400

    3/13 SetBrake() in Tele-Init

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -107,6 +107,7 @@ public class Robot extends TimedRobot {
         // teleop starts running. If you want the autonomous to
         // continue until interrupted by another command, remove
         // this line or comment it out.
+        RobotContainer.getInstance().m_drivetrain.setBrake();
         if (m_autonomousCommand != null) {
             m_autonomousCommand.cancel();
         }
